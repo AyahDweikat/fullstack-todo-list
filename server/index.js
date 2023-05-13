d61f2c0369a1d610366f822fb5eb9907f8910a76
AyahDweikat <dweikatayah99@gmail.com>
2023-05-13 10:53:46 +0300

    chandes in server side

--- server/index.js
+++ server/index.js
@@ -20,6 +20,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+// serve up production assets
+app.use(express.static('client/build'));
+// let the react app to handle any unknown routes 
+// serve up the index.html if express does'nt recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 
 
