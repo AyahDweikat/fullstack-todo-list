7c7dfe27db826507eafcac477879a238f625b86e
AyahDweikat <dweikatayah99@gmail.com>
2023-05-14 13:13:17 +0300

    add port

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.json())
 const PORT = 3000;
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
+  port:3306,
   user: 'root',
   database: 'todolist',
 });
