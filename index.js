01784cc2ab18741c7f91645238a9936d47ed267e
AyahDweikat <dweikatayah99@gmail.com>
2023-05-14 13:29:56 +0300

    --

--- index.js
+++ index.js
@@ -4,13 +4,50 @@ const mysql = require('mysql2');
 const app = express();
 app.use(express.json())
 const PORT = 3000;
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'todolist',
+// });
+
+var db_config = {
   host: 'localhost',
-  port:3306,
-  user: 'root',
-  database: 'todolist',
-  socketPath: '/tmp/mysql.sock',
-});
+    user: 'root',
+    database: 'todolist'
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+
+
+
+
+
+
+
 // app.use((_req, res, next) => {
 //   // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
 //   res.header('Access-Control-Allow-Headers', '*');
